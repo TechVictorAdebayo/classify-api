da875d764428d08342d6b54710f574ea3daa9d58
TechVictorAdebayo <techvictoradebayo@gmil.com>
2026-04-14 15:38:10 +0100

    updated the package.json

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { default: axios } = require('./node_modules/axios/index.d.cts');
+const axios = require('axios');
 const express = require ('express')
 const app = express();
 const PORT = process.env.PORT || 3000;
